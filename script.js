2a26f30e4f519b096eceda06feca8b8cc2e18bbb
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-12 11:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	cont heap=[...arr];
+	heap.sort((a,b)=>a-b);//min heap
+	let totalCost = 0;
+
+	while(head.length>1){
+		const first=heap.shift();
+		const second=heap.shift();
+		const cost=first+second;
+		totalCost+=cost;
+
+		heap.push(cost);
+		heap.sort((a,b)=>a-b);//re sort to maintain min heap 
+	}
   
 }
 
